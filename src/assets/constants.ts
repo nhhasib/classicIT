95e46fac1ac613910102aaf4d7bda2cf6acf6fc5
nhhasib <nafiul.hasan1997@gmail.com>
2023-12-24 16:10:01 +0600

    banner added

---
+++ src/assets/constants.ts
@@ -0,0 +1,69 @@
+export const heroImage =
+  'https://github.com/mamun02090/chat-application/assets/63799967/ca4682f8-570c-46e3-a4c2-5c1b5f76fc12'
+
+export const trendingProducts = [
+  {
+    imageSrc:
+      'https://github.com/mamun02090/chat-application/assets/63799967/ccbd3d7c-c3c3-472e-a435-5192e0088e08',
+    price: '$55.0',
+    productName: 'Product Name',
+    variant: 'variant',
+  },
+  {
+    imageSrc:
+      'https://github.com/mamun02090/chat-application/assets/63799967/719e6d82-2fbb-4456-b2a5-f6e32fb6daa1',
+    price: '$65.0',
+    productName: 'Product Name Two',
+    variant: 'variant two',
+  },
+  {
+    imageSrc:
+      'https://github.com/mamun02090/chat-application/assets/63799967/e739c475-2143-4690-b80b-d9bc7e7af401',
+    price: '$75.0',
+    productName: 'Product Name Three',
+    variant: 'variant three',
+  },
+  {
+    imageSrc:
+      'https://github.com/mamun02090/chat-application/assets/63799967/f0326eec-1223-4a55-8d06-3b314466f1b9',
+    price: '$95.0',
+    productName: 'Product Name four',
+    variant: 'variant four',
+  },
+]
+export const offerProducts = [
+  {
+    imageSrc:
+      'https://github.com/mamun02090/chat-application/assets/63799967/29c68abb-fdff-4942-8b96-0333ed10aa9b',
+    price: '$55.0',
+    productName: 'Product Name',
+    variant: 'variant',
+  },
+  {
+    imageSrc:
+      'https://github.com/mamun02090/chat-application/assets/63799967/af83d88d-3bf0-4683-92f5-d3a182830b35',
+    price: '$65.0',
+    productName: 'Product Name Two',
+    variant: 'variant two',
+  },
+  {
+    imageSrc:
+      'https://github.com/mamun02090/chat-application/assets/63799967/3c1a7bf5-6537-47d9-805f-1b1bed96a384',
+    price: '$75.0',
+    productName: 'Product Name Three',
+    variant: 'variant three',
+  },
+  {
+    imageSrc:
+      'https://github.com/mamun02090/chat-application/assets/63799967/b4188f90-5106-4f38-a0f9-769a6028b7a8',
+    price: '$95.0',
+    productName: 'Product Name four',
+    variant: 'variant four',
+  },
+]
+
+export const heroCarouselImages = [
+  'https://github.com/mamun02090/chat-application/assets/63799967/2aaed783-53d3-44ff-a9d1-697b38cf476c',
+  'https://github.com/mamun02090/chat-application/assets/63799967/3524b592-fa7e-4e28-a573-24c8f705154b',
+  'https://github.com/mamun02090/chat-application/assets/63799967/d59313fa-ce3b-4fba-9ad8-62a27b9adcac',
+]
--- src/component/Pages/Home/Banner/Banner.jsx
+++ src/component/Pages/Home/Banner/Banner.jsx
@@ -1,21 +1,43 @@
-import React from 'react';
-import img from '../../../../../public/images/happy-cute-brunette-caucasian-grad-girl-is-smiling-blurred-class-mates-are.jpg'
+import React, { useEffect } from 'react';
+
+import Slider from 'react-slick';
 
 const Banner = () => {
+
+    const heroCarouselImages = [
+        'https://github.com/mamun02090/chat-application/assets/63799967/2aaed783-53d3-44ff-a9d1-697b38cf476c',
+        'https://github.com/mamun02090/chat-application/assets/63799967/3524b592-fa7e-4e28-a573-24c8f705154b',
+        'https://github.com/mamun02090/chat-application/assets/63799967/d59313fa-ce3b-4fba-9ad8-62a27b9adcac',
+      ]
+
+    const settings = {
+        className: 'center',
+        infinite: true,
+        dots: false,
+        centerPadding: '60px',
+        slidesToShow: 1,
+        swipeToSlide: true,
+        autoplay: true,
+        speed: 500,
+        autoplaySpeed: 3000,
+      }
     return (
         <div>
-            <div id="slide1" className="carousel-item relative w-full">
-                <img src={img} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <div>
-                    <h1 className='mx-8 text-6xl font-bold text-white'>Life the journey get the bright career</h1>
-                        <div>
-                            <input className='bg-white ml-8 mt-6 rounded-l-3xl px-6 py-3 w-4/5' placeholder='Search your colleges' type="search" name="" id="" />
-                            <button className='bg-primary py-3 px-5 text-white font-bold rounded-r-3xl'>Search</button>
-                   </div>
-                    </div>
-    </div>
-  </div> 
+           
+           <Slider {...settings}>
+        {heroCarouselImages.map((image, index) => {
+          return (
+            // <img
+            //   key={index}
+            //   src={image}
+            //   alt="hero"
+            //   width={1440}
+            //   height={810}
+            // />
+            <img src={image} alt="" srcset="" />
+          )
+        })}
+      </Slider>
         </div>
     );
 };
